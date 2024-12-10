c38629cd8f48c3d0d272b4d5dbe1658a1cb7e330
IulianBotnari <iulianbotnari93@gmail.com>
2024-12-10 12:49:17 +0100

    Add gitignore & fil env

--- films_web_app/app.js
+++ films_web_app/app.js
@@ -3,3 +3,10 @@ const server = express()
 const cors = require('cors')
 server.use(cors())
 
+const HOST = 'http://localhost:'
+const PORT = 3000
+
+server.listen(PORT, () => {
+    console.log(`Server running at ${HOST}${PORT}`)
+})
+
