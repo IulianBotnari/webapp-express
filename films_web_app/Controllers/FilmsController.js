2a31ae0eb38e728b9377fc27bc5a48fbc75ca8e5
IulianBotnari <iulianbotnari93@gmail.com>
2024-12-10 14:22:54 +0100

    Finish

--- films_web_app/Controllers/FilmsController.js
+++ films_web_app/Controllers/FilmsController.js
@@ -6,10 +6,11 @@ const index = ('/', (req, res) => {
 
     connection.query(sql, (err, films) => {
         if (err) return res.status(500).json({ err: err })
-        if (films.length === 0) return res.status(404).json({ err: 'book not found' })
+        if (films.length === 0) return res.status(404).json({ err: 'books not found' })
 
         res.json(films)
     })
+
 
 })
 
@@ -21,13 +22,14 @@ const show = ('/:id', (req, res) => {
     const id = req.params.id
 
     connection.query(sql, [id], (err, filmById) => {
-        // if (err) return res.status(404)
+        if (err) return res.status(500).json({ err: err })
+        if (filmById.length === 0) return res.status(404).json({ err: 'book not found' })
 
 
 
         connection.query(sqlFilmsReview, [id], (err, filmsReviews) => {
             if (err) return res.status(500).json({ err: err })
-            if (filmsReviews.length === 0) return res.status(404).json({ err: 'book not found' })
+            if (filmsReviews.length === 0) return res.status(404).json({ err: 'review not found' })
             console.log(filmsReviews);
 
             const movieWithReview = {
@@ -41,6 +43,8 @@ const show = ('/:id', (req, res) => {
 
 
     })
+
+
 
 })
 module.exports = {
