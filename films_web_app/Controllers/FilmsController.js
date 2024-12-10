fe82e2f91f56af8bf6d4d9abfa261eaf2f4620de
IulianBotnari <iulianbotnari93@gmail.com>
2024-12-10 13:50:25 +0100

    Add Route show

--- films_web_app/Controllers/FilmsController.js
+++ films_web_app/Controllers/FilmsController.js
@@ -16,12 +16,28 @@ const index = ('/', (req, res) => {
 const show = ('/:id', (req, res) => {
 
     const sql = 'SELECT * FROM movies WHERE id =?'
+    const sqlFilmsReview = 'SELECT * FROM reviews WHERE movie_id =?'
     const id = req.params.id
 
     connection.query(sql, [id], (err, filmById) => {
         if (err) return res.status(404)
 
-        res.json(filmById[0])
+
+
+        connection.query(sqlFilmsReview, [id], (err, filmsReviews) => {
+            if (err) return res.status(404)
+            console.log(filmsReviews);
+
+            const movieWithReview = {
+                ...filmById[0],
+                reviews: filmsReviews
+            }
+
+            res.json(movieWithReview)
+        })
+
+
+
     })
 
 })
