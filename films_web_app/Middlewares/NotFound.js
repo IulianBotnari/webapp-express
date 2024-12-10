48d309be0c8d04620c91bbe97dcd1dca2d655a78
IulianBotnari <iulianbotnari93@gmail.com>
2024-12-10 13:58:37 +0100

    Add Middlewares

---
+++ films_web_app/Middlewares/NotFound.js
@@ -0,0 +1,6 @@
+const NotFound = (req, res, next) => {
+    res.status(404).json({ err: 'not found' })
+}
+
+
+module.exports = NotFound
--- films_web_app/app.js
+++ films_web_app/app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const server = express()
 const cors = require('cors')
+const NotFound = require('./Middlewares/NotFound.js')
+const ServerErrorsHandler = require('./Middlewares/ServerErrorsHandler.js')
 server.use(cors())
 
 const router = require('./Routes/router.js')
@@ -8,10 +10,15 @@ const router = require('./Routes/router.js')
 const HOST = 'http://localhost:'
 const PORT = 3000
 
-server.listen(PORT, () => {
-    console.log(`Server running at ${HOST}${PORT}`)
-})
+server.use(NotFound)
+
+server.use(ServerErrorsHandler)
 
 server.get('/', router)
 
 server.get('/:id', router)
+
+
+server.listen(PORT, () => {
+    console.log(`Server running at ${HOST}${PORT}`)
+})
